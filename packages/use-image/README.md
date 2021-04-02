207de3c49a41694fb1b1792be401d3e1486ca9bd
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-03 05:45:47 +0900

    add use image

---
+++ packages/use-image/README.md
@@ -0,0 +1,37 @@
+## Installation
+
+```sh
+yarn add @nothing.app/use-image
+```
+
+## Usage
+
+```tsx
+import React from "react";
+import { Image } from "react-konva";
+import useImage from "use-image";
+
+const url =
+  "https://bridged-service-static.s3-us-west-1.amazonaws.com/branding/bridged-logo-512.png";
+
+function SimpleApp() {
+  const [image] = useImage(url);
+
+  // "image" will DOM image element or undefined
+
+  return <Image image={image} />;
+}
+
+function ComplexApp() {
+  // set crossOrigin of image as second argument
+  const [image, status] = useImage(url, "Anonymous");
+
+  // status can be "loading", "loaded" or "failed"
+
+  return <Image image={image} />;
+}
+```
+
+## Related
+
+For Loading CORS Images, you can use https://cors.bridged.cc
---
+++ packages/use-image/index.js
@@ -0,0 +1,43 @@
+var React = require("react");
+
+var defaultState = { image: undefined, status: "loading" };
+
+module.exports = function useImage(url, crossOrigin) {
+  var res = React.useState(defaultState);
+  var image = res[0].image;
+  var status = res[0].status;
+
+  var setState = res[1];
+
+  React.useEffect(
+    function () {
+      if (!url) return;
+      var img = document.createElement("img");
+
+      function onload() {
+        setState({ image: img, status: "loaded" });
+      }
+
+      function onerror() {
+        setState({ image: undefined, status: "failed" });
+      }
+
+      img.addEventListener("load", onload);
+      img.addEventListener("error", onerror);
+      crossOrigin && (img.crossOrigin = crossOrigin);
+      img.src = url;
+
+      return function cleanup() {
+        img.removeEventListener("load", onload);
+        img.removeEventListener("error", onerror);
+        setState(defaultState);
+      };
+    },
+    [url, crossOrigin]
+  );
+
+  // return array because it it better to use in case of several useImage hooks
+  // const [background, backgroundStatus] = useImage(url1);
+  // const [patter] = useImage(url2);
+  return [image, status];
+};
