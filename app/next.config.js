323c6d65ed33b302d5103233272f23e4faedf8e0
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-03 05:50:02 +0900

    remove raw-canvaskit demo

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,3 @@
-const CopyPlugin = require("copy-webpack-plugin");
 const withTM = require("next-transpile-modules")(["@nothing.app/skia-backend"]);
 
 module.exports = withTM({
@@ -6,18 +5,6 @@ module.exports = withTM({
     config.node = {
       fs: "empty",
     };
-    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
-
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: "../node_modules/canvaskit-wasm/bin/canvaskit.wasm",
-          },
-        ],
-      })
-    );
-
     return config;
   },
 });
